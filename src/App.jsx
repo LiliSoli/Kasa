24d7605ca5ba645c0c8f03df74b2611774053e05
Aurélie Solier <solier.lili@gmail.com>
2024-06-07 01:17:41 +0200

    Correction routeur

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Home from './pages/home/Home';
+import Location from './pages/location/Location';
+import About from './pages/about/About';
+import Error from './pages/error/Error';
 import './assets/styles/main.css';
-import AppRouter from './AppRouter';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <AppRouter />
-    </div>
+    <Router>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/location/:id" element={<Location />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
