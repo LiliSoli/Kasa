120e31641392355ad5c1012238bf95d04bafeff2
Aurélie Solier <solier.lili@gmail.com>
2024-06-09 23:43:11 +0200

    Responsive Home ok

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -4,11 +4,13 @@ import './_banner.scss';
 function Banner(props) {
   const backgroundOpacity = props.isAboutPage ? "banner__overlay__about-page" : "banner__overlay__home-page";
 
+  const bannerText = window.innerWidth <= 425 && props.bannerText ? "Chez vous,\npartout et ailleurs" : props.bannerText;
+
   return (
     <div className="banner">
       <img src={props.imageUrl} className="banner__img" alt={props.altText} />
       <div className={`banner__overlay ${backgroundOpacity}`}>
-        <h1 className="banner__overlay__text">{props.bannerText}</h1>
+        <h1 className="banner__overlay__text">{bannerText}</h1>
       </div>
     </div>
   )
--- src/components/collapseItem/CollapseItem.jsx
+++ src/components/collapseItem/CollapseItem.jsx
@@ -1,14 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import collapseIcon from '../../assets/images/collapse-icon.png';
 import './_collapse-item.scss';
 
 function CollapseItem(props) {
     const [isOpen, setIsOpen] = useState(false);
+    const contentRef = useRef(null);
 
     const activeCollapse = () => {
         setIsOpen(!isOpen);
     };
 
+    useEffect(() => {
+        if (isOpen) {
+            contentRef.current.style.maxHeight = `${contentRef.current.scrollHeight}px`;
+        } else {
+            contentRef.current.style.maxHeight = '0px';
+        }
+    }, [isOpen]);
+    
     return (
         <div className="collapse-item">
             <div className="collapse-item__heading">
@@ -20,23 +29,21 @@ function CollapseItem(props) {
                 />
             </div>
 
-            {isOpen && (
-                <div className="collapse-item__content">
-                    {props.description &&
-                        <div>{props.description}</div>
-                    }
-    
-                    {props.equipments &&
-                        <>
-                            {props.equipments.map((equipment, index) => (
-                            <span key={index}>
-                                {equipment}
-                            </span>
-                            ))}
-                        </>
-                    }
-                </div>
-            )}
+            <div ref={contentRef} className={`collapse-item__content ${isOpen ? "open" : "close"}`}>
+                {props.description &&
+                    <div>{props.description}</div>
+                }
+
+                {props.equipments &&
+                    <>
+                        {props.equipments.map((equipment, index) => (
+                        <span key={index}>
+                            {equipment}
+                        </span>
+                        ))}
+                    </>
+                }
+            </div>
         </div>
   )
 };
