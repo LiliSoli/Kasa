207df0703c700dccef4150acc1c8694ced91ac95
Aurélie Solier <solier.lili@gmail.com>
2024-06-10 14:49:34 +0200

    Animation collapseItem

--- src/components/collapseItem/CollapseItem.jsx
+++ src/components/collapseItem/CollapseItem.jsx
@@ -1,47 +1,46 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState } from 'react';
 import collapseIcon from '../../assets/images/collapse-icon.png';
 import './_collapse-item.scss';
 
 function CollapseItem(props) {
     const [isOpen, setIsOpen] = useState(false);
-    const contentRef = useRef(null);
 
     const activeCollapse = () => {
         setIsOpen(!isOpen);
     };
-
-    useEffect(() => {
-        if (isOpen) {
-            contentRef.current.style.maxHeight = `${contentRef.current.scrollHeight}px`;
-        } else {
-            contentRef.current.style.maxHeight = '0px';
-        }
-    }, [isOpen]);
     
     return (
         <div className="collapse-item">
             <div className="collapse-item__heading">
-                <span>{props.title}</span>
-                <img
-                    src={collapseIcon} alt="Flèche bas"
-                    className={`collapse-icon ${isOpen ? "open" : "close"}`}
-                    onClick={activeCollapse}
-                />
+                <div className="collapse-item__heading__title">
+                    <span>{props.title}</span>
+                </div>
+                <div className="collapse-item__heading__icon-content"
+                    onClick={activeCollapse}>
+                    <img
+                        src={collapseIcon} alt="Flèche bas"
+                        className={`collapse-icon ${isOpen ? "open" : "close"}`}
+                    />
+                </div>
             </div>
 
-            <div ref={contentRef} className={`collapse-item__content ${isOpen ? "open" : "close"}`}>
+            <div
+                className={`collapse-item__content ${isOpen ? "open" : "close"}`}
+            >
                 {props.description &&
-                    <div>{props.description}</div>
+                    <div className={`collapse-item__content__text ${isOpen ? "open" : "close"}`}>
+                        {props.description}
+                    </div>
                 }
 
                 {props.equipments &&
-                    <>
+                    <div className={`collapse-item__content__text ${isOpen ? "open" : "close"}`}>
                         {props.equipments.map((equipment, index) => (
                         <span key={index}>
                             {equipment}
                         </span>
                         ))}
-                    </>
+                    </div>
                 }
             </div>
         </div>
